cf427d00fca7acb077ade96e31f3ee48f60a9b05
Jeetendra Singh <chauhanjeetendra@gmail.com>
2025-07-03 23:49:12 +0530

    Add package.json and initial twoSum test implementation

---
+++ src/1-two-sum.test.js
@@ -0,0 +1,20 @@
+import { expect, test } from "@jest/globals";
+
+function twoSum(nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+  return [];
+}
+
+test("twoSum", () => {
+  expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+  expect(twoSum([3, 2, 4], 6)).toEqual([1, 2]);
+  expect(twoSum([3, 3], 6)).toEqual([0, 1]);
+  expect(twoSum([3, 2, 3], 4)).toEqual([]);
+});
